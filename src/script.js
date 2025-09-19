f7af8c5ceff1558f23f62bef94e0ed62a50b9b25
Isabella Harrington <isabellaharrington@gmail.com>
2025-09-18 22:18:48 -0400

    Created launchSearch() function that checks for a search form submission and sends the user to a new page once submitted
    This function will later be edited to verify the validity of the search input

--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
 import './style.css';
 import { loadHome } from './home.js';
+import { loadWeatherPage } from './weather.js';
+
 loadHome();
+
+function launchSearch() {
+    const searchForm = document.getElementById('search-form');
+
+    searchForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        loadWeatherPage();
+    })
+}
+
+launchSearch();
