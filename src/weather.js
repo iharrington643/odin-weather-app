b6609569d12ba82ee3357ca9eda765e32b999bbe
Isabella Harrington <isabellaharrington@gmail.com>
2025-09-18 22:32:55 -0400

    Edited loadWeatherPage() function to display a container that will later store weather data
    Edited styling of container in CSS

--- src/weather.js
+++ src/weather.js
@@ -1,4 +1,8 @@
 export function loadWeatherPage() {
     const siteBody = document.getElementById('site-body');
     siteBody.innerHTML = '';
+
+    const weatherContainer = document.createElement('div');
+    weatherContainer.id = 'weather-container';
+    siteBody.appendChild(weatherContainer);
 }
