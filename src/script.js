7e89b80e1df96a1910ba5b04492d9ad72c19645e
Isabella Harrington <isabellaharrington@gmail.com>
2025-09-19 15:19:06 -0400

    Created asynchronous function which fetches weather information about a location and saves it to a json object
    Set up the search form event listener to run the function using the argument searchInput (the search value)

--- src/script.js
+++ src/script.js
@@ -2,15 +2,35 @@ import './style.css';
 import { loadHome } from './home.js';
 import { loadWeatherPage } from './weather.js';
 
+export const jsonObject = {};
+
 loadHome();
 
 function launchSearch() {
     const searchForm = document.getElementById('search-form');
 
     searchForm.addEventListener('submit', function(event) {
+        const searchInput = document.getElementById('search-bar').value;
+
         event.preventDefault();
         loadWeatherPage();
+        fetchWeather(searchInput);
     })
+}
+
+async function fetchWeather(location) {
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=B9GWLUJEUZVXAPLZ6RJ3G6566`);
+        
+        if (!response.ok) {
+            throw new Error(`Status: ${response.status}`);
+        }
+        const jsonData = await response.json();
+        jsonObject = JSON.parse(jsonData);
+
+    } catch (error) {
+        console.error(error.message);
+    }
 }
 
 launchSearch();
